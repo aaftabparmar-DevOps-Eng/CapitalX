ce927788f80365c45fc7f1928d41f23e47bc1f12
Aaftab_Parmar <aaftabparmar29@gmail.com>
2026-05-30 19:13:12 +0530

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+echo "🐳 Installing Docker..."
 sudo apt update -y
 sudo apt install -y docker.io docker-compose
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ubuntu
 newgrp docker
+echo "✅ Docker installed!"
